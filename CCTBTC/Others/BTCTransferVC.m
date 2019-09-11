0c42f58a5bc59f95d08b4355580b892caa952b9d
TT-struggleofoneself <504470358@qq.com>
2019-09-11 11:26:34 +0800

    更新

--- CCTBTC/Others/BTCTransferVC.m
+++ CCTBTC/Others/BTCTransferVC.m
@@ -33,7 +33,7 @@
 - (IBAction)btnAction:(id)sender {
     
     /**
-     完整的 CCTBTCLib 库代码，请加我的微信：tiantaotaobao 收费400元（ETH+BTC全套库+Demo代码）
+     完整的 CCTBTCLib 库代码，请加我的微信：tiantaotaobao 收费600元（ETH+BTC全套库+Demo代码）
      代码里不明白的都可以问我，其他区块链相关的问题我们也都可以互相讨论，专注区块链开发，还希望交个朋友。
      **/
     
--- CCTBTC/Others/ImportByMVC.m
+++ CCTBTC/Others/ImportByMVC.m
@@ -27,23 +27,29 @@
 - (IBAction)btnAction:(id)sender {
     
     /**
-     完整的 CCTBTCLib 库代码，请加我的微信：tiantaotaobao 收费400元（ETH+BTC全套库+Demo代码）
+     完整的 CCTBTCLib 库代码，请加我的微信：tiantaotaobao 收费600元（ETH+BTC全套库+Demo代码）
      代码里不明白的都可以问我，其他区块链相关的问题我们也都可以互相讨论，专注区块链开发，还希望交个朋友。
      **/
     
-    _mTextV.text = @"suffer hero hole vibrant sketch fish venture insane face regular fun ready";
-    
+    _mTextV.text = @"physical viable material guide school rail lava lock foster success joy flat";
     if(_mTextV.text.length == 0){
         return;
     }
     
-    [CCTBTCLib importWalletByMnemonic:_mTextV.text Newwork:mainnet Path:btcMainnet password:@"123456" resultBlock:^(NSString *private, NSString *address) {
-        
+    [CCTBTCLib importWalletByMnemonic:_mTextV.text Newwork:mainnet Path:btcMainnet password:@"" resultBlock:^(NSString *private, NSString *address) {
         self.walletText.text = [NSString stringWithFormat:@"【私钥】：%@ \n\n 【地址】：%@ \n\n",private,address];
     } error:^{
         
         NSLog(@"导入失败");
     }];
+    
+    
+    
 }
+
+
+
+
+
 
 @end
